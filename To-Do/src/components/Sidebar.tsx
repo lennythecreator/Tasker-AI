f6c91b077e46dd1a38860c74fd8220cdff95a7c9
Lenny Uwaeme <48912140+lennythecreator@users.noreply.github.com>
2024-07-12 23:05:47 -0400

    Updated Todo.tsx

--- To-Do/src/components/Sidebar.tsx
+++ To-Do/src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export const Sidebar = () => {
   return (
-    <div className='w-56 bg-blue-800 h-[90vh] p-2'>
+    <div className='w-56 bg-blue-800 h-[100vh] p-2'>
         <ul className='flex flex-col gap-2 p-2 m-2'>
              <Link to='/home'>Home</Link>
              <Link to='/MyTasks'>Tasks</Link>
--- To-Do/src/components/todo.tsx
+++ To-Do/src/components/todo.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const Todo = () => {
   return (
-    <div className='w-[60vw] h-[80vh] bg-slate-400 rounded-lg mr-auto ml-auto'></div>
+    <div className='w-[60vw] h-[80vh] bg-white rounded-lg mr-auto ml-auto  border-[2px] border-slate-300'>
+      <main className='grid grid-cols-[6fr_5fr] grid-rows-[59px_1fr] overflow-hidden'>
+      <header className='col-[1/3] row-[1/2] bg-[#cfdafa]'></header>
+      <ul></ul>
+
+      <section className='border-l-2 border-l-black-[0.08]'>ok</section>
+      </main>
+    </div>
   )
 }
